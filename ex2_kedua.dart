433a11a0e5a580d812a8873e1302e6490d101359
ruhayahayuni <150129899+ruhayahayuni@users.noreply.github.com>
2024-03-14 13:33:33 +0700

    Ruhaya Hayuni

---
+++ ex2_kedua.dart
@@ -0,0 +1,5 @@
+void main(List<String> buahbuahan) {
+for(String buah in buahbuahan) {
+print(buah);
+}
+}
---
+++ ex3_ketiga.dart
@@ -0,0 +1,6 @@
+void main() {
+  String polbeng = 'Politeknik Negeri Bengkalis';
+  String ti = 'Teknik Informatika';
+  String rpl = 'Rekayasa Perangkat Lunak';
+  print('Saya kuliah di $polbeng jurusan $ti program studi $rpl');
+}
